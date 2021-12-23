a0cc91c594fad73e7a93043b73379ef08d3c44bc
hideyukikanazawa <kanazawahideyuki@gmail.com>
2021-12-23 23:12:48 +0800

    Initial commit

---
+++ util/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	OPENAPI_URL string `mapstructure:"OPENAPI_URL"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
